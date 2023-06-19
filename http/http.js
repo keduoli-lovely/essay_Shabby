8202cb2075c15bab06316168b21539a88fdd1b8d
keduoli-lovely <95008862+keduoli-lovely@users.noreply.github.com>
2023-06-20 01:31:32 +0800

    token过期,跳转到login页面,暂时不做无感刷新token

--- http/http.js
+++ http/http.js
@@ -28,6 +28,12 @@ http.interceptors.response.use(function (response) {
   }, function (error) {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+	if(error.response.status == 401) {
+		uni.removeStorageSync('userinfo')
+		uni.reLaunch({
+			url: '/pages/login/login'
+		})
+	}
     return Promise.reject(error);
   });
   
