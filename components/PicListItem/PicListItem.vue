5236977ad2467ad2e749de88800acd503739db70
keduoli-lovely <95008862+keduoli-lovely@users.noreply.github.com>
2023-06-21 16:17:32 +0800

    图片小组件

---
+++ components/PicListItem/PicListItem.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="pic">
+		<image :src="pic" mode="widthFix"></image>
+	</view>
+</template>
+
+<script setup>
+	
+	defineProps({
+		pic: String,
+	})
+
+</script>
+
+<style lang="scss" scoped>
+	.pic {
+		margin-top: 20rpx;
+		padding: 20rpx;
+		width: 100%;
+		background-color: #D2D2D2;
+		border-radius: 4rpx;
+		image {
+			width: 100%;
+		}
+	}
+</style>
